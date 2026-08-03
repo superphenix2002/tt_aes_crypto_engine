5d7c8cbde8fa622cac48bf68a2a4d41433ac94cb
Anirban <superphenix2002@gmail.com>
2026-08-03 09:14:53 +0530

    Update mix_cols_decrypt.sv

--- src/mix_cols_decrypt.sv
+++ src/mix_cols_decrypt.sv
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2024 Anirban Sarkar
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+`default_nettype none
+
+
 module mix_cols_inverse(input logic [7:0] data_mat00,
 input logic [7:0] data_mat01,
 input logic [7:0] data_mat02,
