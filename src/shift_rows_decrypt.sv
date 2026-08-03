80fe98a8942ecad4d53c525fc7dca98f6698cb67
Anirban <superphenix2002@gmail.com>
2026-08-03 09:15:40 +0530

    Update shift_rows_decrypt.sv

--- src/shift_rows_decrypt.sv
+++ src/shift_rows_decrypt.sv
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2024 Anirban Sarkar
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+`default_nettype none
+
+
 module shift_rows_inverse(
 input logic [7:0] data_mat00,
 input logic [7:0] data_mat01,
